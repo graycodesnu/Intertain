90e1e887a7c3da30313c78791d9948c698594d58
Aumio Aziz <aumio.aziz@gmail.com>
2022-07-21 18:42:08 -0700

    added loop for 5 movies added variables to to make code more readable

--- movieAPItest/testing.js
+++ movieAPItest/testing.js
@@ -1,10 +1,14 @@
 var zone = document.querySelector("#movieZone")
 
 var url = //"https://api.themoviedb.org/3/genre/movie/list?api_key=e346bd747060c7a18ce3926d8f5571b9&language=en-US"
-        "https://api.themoviedb.org/3/discover/movie?api_key=e346bd747060c7a18ce3926d8f5571b9&with_genres=18&page=3"
+        "https://api.themoviedb.org/3/discover/movie?api_key=e346bd747060c7a18ce3926d8f5571b9&with_genres=18&page=1"
         //  "http://openlibrary.org/subjects/genre.json"
 
 var posterPath = "https://image.tmdb.org/t/p/original/"
+
+// var boardAPI = "https://api.boardgameatlas.com/api/search?name=Catan&pretty=true&client_id=rHFhdFeTrQ&categories=aliens"
+
+
 // var pastGenre = 'love'
 // url = url.replace('genre',pastGenre) 
 
@@ -14,7 +18,21 @@ fetch(url)
     })
     .then(function (data) {
         console.log(data)
-        zone.children[0].textContent = data.results[0].title
-        zone.children[1].src = posterPath.concat(data.results[0].poster_path)
-        zone.children[1].setAttribute('style','width:25%')
+
+        for(i = 0; i < 5; i++){
+            var title = document.createElement('h2')
+            var poster = document.createElement('img')
+            var description = document.createElement('p')
+
+            var currentMovie = data.results[i]
+
+            title.textContent = currentMovie.title
+            poster.src = posterPath.concat(currentMovie.poster_path)
+            poster.setAttribute('style','width:25%')
+            description.textContent = currentMovie.overview
+
+            zone.appendChild(title)
+            zone.appendChild(poster)
+            zone.appendChild(description)
+        }
     })
