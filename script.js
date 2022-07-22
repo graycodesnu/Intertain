e7b37f5b3959149581521dd2b82ef72a7af59a21
Holly Parker <hollygcochran@gmail.com>
2022-07-22 11:43:35 -0500

    getting games to page

--- script.js
+++ script.js
@@ -5,33 +5,39 @@ var gameSearchbtn = document.querySelector("#boardGameSearch")
 console.log('hello!')
 function getGame() {
     var apiKey = "j93pbu8wKv"
-    var requestUrl = "https://api.boardgameatlas.com/api/game/categories?pretty=true&client_id=" + apiKey
+    // var requestUrl = "https://api.boardgameatlas.com/api/game/categories?pretty=true&client_id=" + apiKey
+    var requestUrl = "https://api.boardgameatlas.com/api/search?order_by=rank&ascending=false&client_id=j93pbu8wKv"
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
     })
     .then(function (data) {
         console.log(data)
+
         console.log('hi!')
 
-        // document.querySelector
+    // getting the first 5 games on the api
+    // todo filter by categories 
+        for(i = 0; i < 5; i++){
+            console.log("IN FOR LOOP")
+            var title = document.createElement('h2')
+            var poster = document.createElement('img')
+            var description = document.createElement('p')
 
-        // for(i = 0; i < 5; i++){
-        //     var title = document.createElement('h2')
-        //     var poster = document.createElement('img')
-        //     var description = document.createElement('p')
+            var currentGame = data.games[i]
+            console.log(currentGame)
+            title.textContent = currentGame.name
+            poster.src = (currentGame.image_url)
+            poster.setAttribute('style','width:25%')
+            description.textContent = currentGame.description
 
-        //     var currentMovie = data.results[i]
+            game.appendChild(title)
+            game.appendChild(poster)
+            game.appendChild(description)
+        }
 
-        //     title.textContent = currentMovie.title
-        //     poster.src = posterPath.concat(currentMovie.poster_path)
-        //     poster.setAttribute('style','width:25%')
-        //     description.textContent = currentMovie.overview
 
-        //     zone.appendChild(title)
-        //     zone.appendChild(poster)
-        //     zone.appendChild(description)
-     
+
         })
     }
 
