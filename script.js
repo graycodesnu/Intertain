11e67ed0f4b1d5b65995fb5ec22785c6ef96abd2
Holly Parker <hollygcochran@gmail.com>
2022-07-23 15:13:51 -0500

    getting random and only one game back from api

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ var gameSearchbtn = document.querySelector("#boardGameSearch")
 
 
 console.log('hello!')
+
+// todo add a list of categories
+category = []
+
+
+var family = document.getElementById('categories').value = "Family Game"
+console.log(family)
 function getGame() {
     var apiKey = "j93pbu8wKv"
-    // var requestUrl = "https://api.boardgameatlas.com/api/game/categories?pretty=true&client_id=" + apiKey
-    var requestUrl = "https://api.boardgameatlas.com/api/search?order_by=rank&ascending=false&client_id=j93pbu8wKv"
+    // var requestUrl = "https://www.boardgameatlas.com/api/category=7rV11PKqME&client_id=j93pbu8wKv"
+    // var requestUrl = "https://api.boardgameatlas.com/category/7rV11PKqME/family-game?categories=7rV11PKqME"
+    var requestUrl = "https://api.boardgameatlas.com/api/search?order_by=rank&family-gameascending=false&limit=1&random=true&client_id=j93pbu8wKv"
+    
+    console.log(requestUrl)
+    
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
@@ -15,10 +26,12 @@ function getGame() {
         console.log(data)
 
         console.log('hi!')
+        console.log(Object)
 
-    // getting the first 5 games on the api
+    // getting the first 10 games on the api
     // todo filter by categories 
-        for(i = 0; i < 5; i++){
+    // todo need to randomize which games it pulls
+        for(i = 0; i < 11; i++){
             console.log("IN FOR LOOP")
             var title = document.createElement('h2')
             var poster = document.createElement('img')
@@ -43,3 +56,6 @@ function getGame() {
 
 
 gameSearchbtn.addEventListener("click",getGame)
+
+// * if categories.name === Family Game, 
+// * then 
