3ebb92c48b258267dadb16543c94c6db34bcc8ee
Holly Parker <hollygcochran@gmail.com>
2022-07-25 13:02:55 -0500

    pulling games for rest of categories

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ var travel = document.getElementById('categories').value = "Travel"
 var adventure = document.getElementById('categories').value = "Adventure"
 
 
-
+// todo history and biographical isnt working
     
 function getGame() {
 // &random=true
@@ -35,12 +35,25 @@ function getGame() {
     }else if(family === categoriesDD.value) {
         console.log("FAM TIME g rated only")
         var requestUrl = "https://api.boardgameatlas.com/api/search?categories=7rV11PKqME&client_id=j93pbu8wKv"
-    }
+    } else if(sciFi === categoriesDD.value) {
+        console.log("sci fi aliens")
+        var requestUrl = "https://api.boardgameatlas.com/api/search?categories=3B3QpKvXD3&client_id=j93pbu8wKv"
+    }  else if(science === categoriesDD.value) {
+        console.log("bill nye science guy")
+        var requestUrl = "https://api.boardgameatlas.com/api/search?categories=GCwXmvBCvA&client_id=j93pbu8wKv"
+    } else if(food === categoriesDD.value) {
+        console.log("fruit salad yummy yummy")
+        var requestUrl = "https://api.boardgameatlas.com/api/search?categories=YrDuNj8lvr&client_id=j93pbu8wKv"
+    } else if(travel === categoriesDD.value) {
+        console.log("travel")
+        var requestUrl = "https://api.boardgameatlas.com/api/search?categories=TR4CiP8Huj&client_id=j93pbu8wKv"
+    } else if(adventure === categoriesDD.value) {
+        console.log("travel")
+        var requestUrl = "https://api.boardgameatlas.com/api/search?categories=KUBCKBkGxV&client_id=j93pbu8wKv"
+    } 
    
     var apiKey = "j93pbu8wKv"
-    // var requestUrl = "https://api.boardgameatlas.com/api/search?categories=7rV11PKqME&client_id=j93pbu8wKv"
-
-    console.log(requestUrl)
+   
     
     fetch(requestUrl)
         .then(function (response) {
@@ -52,7 +65,6 @@ function getGame() {
         console.log('hi!')
         console.log(Object)
 
-    // getting the first 10 games on the api
     // todo need to randomize which games it pulls
         for(i = 0; i < 1; i++){
             // console.log("IN FOR LOOP")
