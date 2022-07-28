634d1f3685a3ceb36aeb04a0b40c8a7d41a3b265
aaaziz2 <aumio.aziz@gmail.com>
2022-07-27 23:19:59 -0500

    Removed icon placeholders and fixed script tags on each of the favorite HTMLs wrote JS to append first 4 items of each medium in localStorage to the page JS will trigger only on the page that needs it

--- favorite-results/favorite-results.js
+++ favorite-results/favorite-results.js
@@ -1,28 +1,69 @@
-console.log("connected")
-var favBook = document.getElementById("#favBook")
-// Book
-var bookStorage = window.localStorage.getItem('BOOK');
-var bookTitle = localStorage.getItem("BOOK")
+var favBook = document.querySelector("#favBook")
 
-console.log(bookStorage);
-const jsonBook = JSON.parse(bookTitle)
-console.log(bookTitle)
-var title = bookTitle.title
-console.log(title)
-var title = document.createElement("h4");
-var poster = document.createElement("img");
-// todo get specific item from arr from local storage
+// https://www.javascripttutorial.net/web-apis/javascript-localstorage/
+// get list of names from localStorage
+let keys = Object.keys(localStorage)
+
+// https://bobbyhadz.com/blog/javascript-check-if-array-contains-substring-match#check-if-array-contains-a-substring-using-filter
+var favBooks = keys.filter(element => element.includes('BOOK'))
+console.log(favBooks)
+
+var booksGenerated = false;
+if(favBook){
+    for(var i = 0; i < favBooks.length && i < 4; i++){
+        var title = document.createElement("h4")
+        var poster = document.createElement("img")
+
+        var book = localStorage.getItem(favBooks[i]).split(',')
+
+        title.textContent = book[0]
+        poster.src = book[1]
+        poster.setAttribute('style','width: 100%')
+
+        favBook.children[i].appendChild(title)
+        favBook.children[i].appendChild(poster)
+    }
+}
+
+var favMovie = document.querySelector("#favMovie")
+
+var favMovies = keys.filter(element => element.includes('MOVIE'))
+console.log(favMovies)
 
 
-// append to page
-// favBook.appendChild(title)
-// favBook.appendChild(poster)
+if(favMovie){
+    for(var i = 0; i < favMovies.length && i < 4; i++){
+        var title = document.createElement("h4")
+        var poster = document.createElement("img")
 
+        var movie = localStorage.getItem(favMovies[i]).split(',')
 
-// Movie
-var movieStorage =window.localStorage.getItem("MOVIE");
-console.log(movieStorage)
+        title.textContent = movie[0]
+        poster.src = movie[1]
+        poster.setAttribute('style','width: 100%')
 
-// Game
-var gameStorage = window.localStorage.getItem('GAME');
-console.log(gameStorage)
+        favMovie.children[i].appendChild(title)
+        favMovie.children[i].appendChild(poster)
+    }
+}
+
+var favGame = document.querySelector("#favGame")
+
+var favGames = keys.filter(element => element.includes('GAME'))
+console.log(favGames)
+
+if(favGame){
+    for(var i = 0; i < favGames.length && i < 4; i++){
+        var title = document.createElement("h4")
+        var poster = document.createElement("img")
+
+        var game = localStorage.getItem(favGames[i]).split(',')
+
+        title.textContent = game[0]
+        poster.src = game[1]
+        poster.setAttribute('style','width: 100%')
+
+        favGame.children[i].appendChild(title)
+        favGame.children[i].appendChild(poster)
+    }
+}
